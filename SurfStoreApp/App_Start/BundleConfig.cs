98ad4b9025d187a897bf84b9c3ac5d24841079e9
hanwang <hanwang.work@gmail.com>
2014-03-31 02:33:41 +0800

    support gzip for proxy.

--- SurfStoreApp/App_Start/BundleConfig.cs
+++ SurfStoreApp/App_Start/BundleConfig.cs
@@ -6,13 +6,13 @@ namespace SurfStoreApp
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            BundleTable.EnableOptimizations = true;
-
             bundles.Add(new StyleBundle("~/styles/css")
                 .Include("~/content/css/bootstrap.css", "~/content/css/bootstrap-responsive.css"));
 
             bundles.Add(new ScriptBundle("~/scripts/js")
                 .Include("~/Scripts/jquery-1.7.2.js", "~/Scripts/bootstrap.alert.js"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
