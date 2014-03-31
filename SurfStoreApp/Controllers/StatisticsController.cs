0f97dfb27d842479be344a4305f919a2483eac56
hanwang <hanwang.work@gmail.com>
2014-04-01 02:03:29 +0800

    HTML5 web worker example.

---
+++ SurfStoreApp/Controllers/StatisticsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Web.Mvc;
+
+namespace SurfStoreApp.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public ActionResult Update()
+        {
+            Thread.Sleep(5000);
+
+            return Json("Success", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ SurfStoreApp/Scripts/worker.js
@@ -0,0 +1,8 @@
+﻿var request = new XMLHttpRequest();
+request.open('POST', '/statistics/update', false);
+request.send(null);
+
+if (request.status == 200) {
+    // return a message to the calling script
+    postMessage(request.responseText);
+}
