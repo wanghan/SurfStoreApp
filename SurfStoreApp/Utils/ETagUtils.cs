69a00b08f901629fa913ddfa814bb46fa4676922
hanwang <hanwang.work@gmail.com>
2014-04-02 22:29:16 +0800

    remove the Etag

---
+++ SurfStoreApp/Utils/ETagUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurfStoreApp.Utils
+{
+    public class ETagUtils : IHttpModule
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            // Occurs when ASP.NET has completed executing all request event handlers and the request state data has been stored.
+            context.PostReleaseRequestState += Application_PostReleaseRequestState;
+        }
+
+        void Application_PostReleaseRequestState(object sneder, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("ETag");
+        }
+    }
+}
