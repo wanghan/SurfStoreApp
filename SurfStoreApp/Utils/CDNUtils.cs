fa734f9d7999da6479d87c8b59567783aa2ff26b
hanwang <hanwang.work@gmail.com>
2014-04-04 03:07:14 +0800

    enable azure cdn

---
+++ SurfStoreApp/Utils/CDNUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Web.Mvc;
+
+namespace SurfStoreApp.Utils
+{
+    public static class CDNUtils
+    {
+        public static MvcHtmlString CDNUrl(this HtmlHelper helper, string contentPath)
+        {
+            contentPath = Path.GetFileName(contentPath);
+
+            string cdnEndpoint = ConfigurationManager.AppSettings["CDNUrl"];
+
+            Uri combinedUri = new Uri(new Uri(cdnEndpoint), contentPath);
+
+            var url = new UrlHelper(helper.ViewContext.RequestContext);
+
+            return new MvcHtmlString(url.Content(combinedUri.ToString()));
+        }
+    }
+}
